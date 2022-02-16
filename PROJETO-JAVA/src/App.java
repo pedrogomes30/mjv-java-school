269220d674efd2082bbb6def7c0f11456bd80003
Pedro Gomes <pgs_cf@hotmail.com>
2022-02-16 19:51:45 -0300

    alterando menssagem de primeiro repositório

--- PROJETO-JAVA/src/App.java
+++ PROJETO-JAVA/src/App.java
@@ -1,5 +1,6 @@
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá turma");
+        System.out.println("primeiro repositório");
     }
 }
